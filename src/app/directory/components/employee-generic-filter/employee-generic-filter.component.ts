ff5ca46e0d19a81aeebac711a4d7ffcfe0e9f909
Byron Cueva <bcueva@ec.krugercorp.com>
2021-07-04 21:53:38 -0500

    Component for generic filter

--- src/app/directory/components/employee-generic-filter/employee-generic-filter.component.ts
+++ src/app/directory/components/employee-generic-filter/employee-generic-filter.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
+/**
+ * Filter generic
+ *
+ * @author bcueva
+ * @version 1.0
+ */
 @Component({
-  selector: 'app-employee-generic-filter',
-  templateUrl: './employee-generic-filter.component.html',
-  styleUrls: ['./employee-generic-filter.component.scss']
+  selector: "app-employee-generic-filter",
+  templateUrl: "./employee-generic-filter.component.html",
+  styleUrls: ["./employee-generic-filter.component.scss"],
 })
-export class EmployeeGenericFilterComponent implements OnInit {
+export class EmployeeGenericFilterComponent {
+  @Input() query!: string;
+  @Output() clickSearch: EventEmitter<string> = new EventEmitter();
+  @Output() clickAdvancedFilters: EventEmitter<void> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  get hasValue(): boolean {
+    return this.query.length > 0;
   }
 
+  clickSearchHandler(): void {
+    this.clickSearch.emit(this.query);
+  }
+
+  clickAdvancedFilersHandler(): void {
+    this.clickAdvancedFilters.emit();
+  }
+
+  clickClearHandler(): void {
+    this.query = "";
+    this.clickSearch.emit(this.query);
+  }
 }
