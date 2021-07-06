92b8ebb8325462581c822a659cc95b72a4a8b543
Byron Cueva <bcueva@ec.krugercorp.com>
2021-07-06 16:44:00 -0500

    Resolve regex

--- src/app/core/constants/validations.constant.ts
+++ src/app/core/constants/validations.constant.ts
@@ -1,5 +1,5 @@
 export const VALIDATIONS = {
   EMAIL_REGEX: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
   NUMBER_REGEX: /^\d+$/,
-  ONLY_LETTERS_REGEX: /^[a-zA-Z]+$/
+  ONLY_LETTERS_REGEX: /^[a-zA-Z\u00C0-\u00FF\s]*$/
 }
